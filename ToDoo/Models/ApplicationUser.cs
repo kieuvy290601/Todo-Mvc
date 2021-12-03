a94c27d8e4c04d9e8898789f2bae8c23796a63ad
kieuvy29 <vy10b3.thptgl@gmail.com>
2021-12-03 19:55:31 +0700

    Base project

---
+++ ToDoo/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoo.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ ToDoo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ToDoo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
